9cd1352de621bd83540254a1d4ed9a2542aa2f5f
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-02-16 18:38:43 +0900

    Title: 부분 수열의 합, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240216/부분 수열의 합/the-sum-of-the-subsequences.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+#define MAX 10005
+
+int n,m;
+int dp[MAX];
+int num[MAX];
+
+
+
+int main() {
+    cin >> n >> m;
+
+    for(int i=0; i<n; i++){
+        cin >> num[i];
+    }
+
+
+    dp[0] = 1;
+
+    for(int i=0; i<n; i++){
+        for(int j=m; 0<j; j--){
+            if(0 <= j-num[i] && dp[j-num[i]] != 0){
+                dp[j] += dp[j-num[i]];
+            }
+        }
+    }
+
+    string ans = 0 < dp[m] ? "Yes" : "No";
+    cout << ans << "\n";
+
+    return 0;
+}
